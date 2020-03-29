a9d0671cf23525d5e24627bf8ac27e0db9df916a
AncApple <inisienoyakiringo@gmail.com>
2020-03-30 03:32:38 +0900

    update sudoku エラー・ルール違反以外実装

--- sudoku.go
+++ sudoku.go
@@ -36,22 +36,62 @@ func (g *Grid) Set(row, column int, digit int8) error {
 	return nil
 }
 
-//func (g *Grid)Clear(){}
+func (g *Grid) Clear(row, column int) error {
+	g[row][column].digit = empty
+	return nil
+}
 
-//func inRow(){}
+func inBounds(row, column int) bool {
+	if row < 0 || row >= rows || column >= columns {
+		return false
+	}
+	return true
+}
 
-//func inColumn(){}
+func validDigit(digit int8) bool {
+	return digit >= 1 && digit <= 9
+}
 
-//func inRegin(){}
+func (g *Grid) inRow(row int, digit int8) bool {
+	for c := 0; c < columns; c++ {
+		if g[row][c].digit == digit {
+			return true
+		}
+	}
+	return false
+}
 
-//func inFixed(){}
+func (g *Grid) inColumn(column int, digit int8) bool {
+	for r := 0; r < rows; r++ {
+		if g[r][column].digit == digit {
+			return true
+		}
+	}
+	return false
+}
+
+func (g *Grid) inRegin(row, column int, digit int8) bool {
+	startRow, startColumn := row/3*3, column/3*3
+	for r := startRow; r < startRow+3; r++ {
+		for c := startColumn; c < startColumn+3; c++ {
+			if g[r][c].digit == digit {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (g *Grid) inFixed(row, column int) bool {
+	return g[row][column].fired
+}
 
 func main() {
 	//NewCreate実行
 	s := NewCreate([rows][columns]int8{
-		{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 9},
+		{2, 3, 0, 0, 0, 0, 0, 0, 0},
+		{2, 0, 3, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0, 0},
